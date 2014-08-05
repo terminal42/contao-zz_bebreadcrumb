6af3fc3f743c20ff73ca559410384248b36734ee
Andreas Schempp <andreas.schempp@terminal42.ch>
2014-08-05 09:11:01 +0200

    Updated copyright statement

--- config/autoload.php
+++ config/autoload.php
@@ -1,13 +1,12 @@
 <?php
 
 /**
- * Contao Open Source CMS
- * 
- * Copyright (C) 2005-2013 Leo Feyer
- * 
- * @package Faq
- * @link    https://contao.org
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ * zz_bebreadcrumb Extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2009-2014, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       http://github.com/terminal42/contao-zz_bebreadcrumb
  */
 
 
